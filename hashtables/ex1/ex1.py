ecf25d8b466abd00fda57dd6c870c7239c50b067
kroaix <kroaix@gmail.com>
2020-10-12 17:52:38 -0400

    MVP and Stretch

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,21 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+
+    for i in range(length):
+        if weights[i] not in cache:
+            cache[weights[i]] = i
+
+    for i in range(length):
+        target = limit - weights[i]
+    
+        if target in cache:
+            if cache[target] == i:
+                pass
+            elif cache[target] > i:
+                return [cache[target], i]
+            else:
+                return [i, cache[target]]
 
     return None
--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,14 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    result = []
+
+    for num in a:
+        if num * -1 not in cache:
+            cache[num] = num
+        else:
+            result.append(abs(num))
 
     return result
 
